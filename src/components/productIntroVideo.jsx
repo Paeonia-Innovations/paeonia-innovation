a42cc79c17e332fa293d650130f14e41811e6c33
PaingSuSan <149450937+PaingSuSan@users.noreply.github.com>
2024-11-25 15:41:42 +0800

    updated router for product page and home page and also added video thumbnil

--- src/components/productIntroVideo.jsx
+++ src/components/productIntroVideo.jsx
@@ -1,17 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/productIntroVideo.css";
 
 export const ProductIntroVideo = (props) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handlePlay = (event) => {
+    setIsPlaying(true);
+    event.preventDefault();
+  };
   return (
     <div id="productIntroVideo">
       <div className="container">
         <div className="col-md-12">
-          <video className="product-video" controls>
+          <h2 className="video-header">Production Introduction Video</h2>
+          {/* <div className="video-container"> */}
+
+          <div className="video-wrapper">
+            {!isPlaying ? (
+              <div
+                className="thumbnail-container"
+                onClick={handlePlay}
+                role="button"
+                and
+                aria-label="Play Video"
+              >
+                <img
+                  src={`${process.env.PUBLIC_URL}/img/PIVideoImage2.png`} // Thumbnail image
+                  alt="Video Thumbnail"
+                  className="video-thumbnail"
+                />
+                <div className="play-button"></div>
+              </div>
+            ) : (
+              <div className="video-placeholder">
+                <video className="product-video" controls autoPlay>
+                  <source
+                    src={`${process.env.PUBLIC_URL}/SpectrometerProductVideo.mp4`}
+                    type="video/mp4"
+                  />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+            )}
+          </div>
+
+          {/* <video className="product-video" controls>
             <source
-              src={`${process.env.PUBLIC_URL}/productVideo.mp4`}
+              src={`${process.env.PUBLIC_URL}/SpectrometerProductVideo.mp4`}
               type="video/mp4"
             />
-          </video>
+          </video> */}
+
+          {/* <div className="video-label">
+            Mid IR Spectrometer Product Showcase
+          </div> */}
+          {/* </div> */}
         </div>
       </div>
     </div>
